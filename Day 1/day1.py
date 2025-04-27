be690581c2cab99a2bc58516f1598bc821a9dc65
Ali <amehdizade111@gmail.com>
2025-04-27 19:58:07 +0400

    Day 1 completed: Variables and Data Types

---
+++ Day 1/day1.py
@@ -0,0 +1,12 @@
+first_name = "Ali"
+last_name = "Mehdizade"
+age = 17
+country = "Azerbaijan"
+print("First name:", first_name)
+print("Last name:", last_name)
+print("Age:", age)
+print("Country:", country)
+print(type(first_name))
+print(type(last_name))
+print(type(age))
+print(type(country))
